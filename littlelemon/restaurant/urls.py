29b58aef6752cd3ff98fe1433533da09dbc507d1
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2024-02-11 11:14:20 -0500

    add serializers

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin 
 from django.urls import path 
 from . import views
-  
+from rest_framework.routers import DefaultRouter
+from .views import MenuItemView, BookingViewSet
+
 urlpatterns = [ 
    #path('', sayHello, name='sayHello'), 
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,31 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics, viewsets, permissions
+from .models import Menu, Booking
+from rest_framework.decorators import api_view, permission_classes, throttle_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+
 # Create your views here.
 
-# def sayHello(request):
-#  return HttpResponse('Hello World')
 def index(request):
- return render(request, 'index.html', {})
+   return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+     queryset = Menu.objects.all()
+     serializer_class = MenuSerializer
+      
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    #permission_classes = [IsAuthenticated]
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
